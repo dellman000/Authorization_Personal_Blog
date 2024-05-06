129ae073e9bfee414dfed0d5bfb0a307885294a1
kendell <kenren@udel.edu>
2024-05-06 09:39:16 -0400

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL,{
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
